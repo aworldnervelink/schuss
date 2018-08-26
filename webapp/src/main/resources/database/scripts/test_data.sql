377605d223858d36075341e026be232523d18f98
Kevin A. Roll <kroll@appropel.com>
2018-08-25 22:04:25 -0400

    Tweaks to layout.

--- webapp/src/main/resources/database/scripts/test_data.sql
+++ webapp/src/main/resources/database/scripts/test_data.sql
@@ -1,3 +1,6 @@
 INSERT INTO public.rental_provider(
 	id, name, logo_url)
-	VALUES (1, 'King Pine', '');
+	VALUES  (1, 'King Pine', ''),
+	        (2, 'Wildcat', ''),
+	        (3, 'Bretton Woods', ''),
+	        (4, 'Loon', '');
