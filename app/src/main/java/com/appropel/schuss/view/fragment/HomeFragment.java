ff7251156f11b507a82379b4bc0d221211664b96
Kevin A. Roll <kroll@appropel.com>
2018-11-14 19:06:35 -0500

    Show name of provider on home screen.

--- app/src/main/java/com/appropel/schuss/view/fragment/HomeFragment.java
+++ app/src/main/java/com/appropel/schuss/view/fragment/HomeFragment.java
@@ -1,12 +1,16 @@
 package com.appropel.schuss.view.fragment;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.appropel.schuss.R;
 import com.appropel.schuss.common.util.EventBusFacade;
@@ -14,6 +18,7 @@ import com.appropel.schuss.controller.SchussController;
 import com.appropel.schuss.controller.UserInterface;
 import com.appropel.schuss.dagger.DaggerWrapper;
 import com.appropel.schuss.model.read.Person;
+import com.appropel.schuss.model.read.Request;
 import com.appropel.schuss.model.read.User;
 import com.appropel.schuss.view.util.PersonItem;
 import com.xwray.groupie.GroupAdapter;
@@ -37,6 +42,10 @@ public final class HomeFragment extends Fragment
     @BindView(R.id.person_list)
     RecyclerView personView;
 
+    /** List of rental requests. */
+    @BindView(R.id.rental_list)
+    ListView rentalView;
+
     /** User interface. */
     @Inject
     UserInterface userInterface;
@@ -53,7 +62,10 @@ public final class HomeFragment extends Fragment
     private Unbinder unbinder;
 
     /** View adapter. */
-    private GroupAdapter adapter;
+    private GroupAdapter personAdapter;
+
+    /** Request view adapter. */
+    private ArrayAdapter<Request> requestAdapter;
 
     /** User data. */
     private User user;
@@ -65,9 +77,12 @@ public final class HomeFragment extends Fragment
         unbinder = ButterKnife.bind(this, view);
         DaggerWrapper.INSTANCE.getComponent().inject(this);
 
-        adapter = new GroupAdapter();
-        personView.setAdapter(adapter);
+        personAdapter = new GroupAdapter();
+        personView.setAdapter(personAdapter);
         personView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        requestAdapter = new RequestAdapter(getActivity(), android.R.layout.simple_list_item_1);
+        rentalView.setAdapter(requestAdapter);
 
         return view;
     }
@@ -87,11 +102,14 @@ public final class HomeFragment extends Fragment
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void onUserEvent(final User event)
     {
-        adapter.clear();
+        personAdapter.clear();
         for (Person person : event.getPersons())
         {
-            adapter.add(new PersonItem(person, userInterface, getActivity()));
+            personAdapter.add(new PersonItem(person, userInterface, getActivity()));
         }
+        requestAdapter.clear();
+        requestAdapter.addAll(event.getRequests());
+        requestAdapter.notifyDataSetChanged();
         user = event;
     }
 
@@ -125,5 +143,29 @@ public final class HomeFragment extends Fragment
     {
         unbinder.unbind();
         super.onDestroyView();
+    }
+
+    /** Quick and dirty adapter to show rental requests. */
+    public static final class RequestAdapter extends ArrayAdapter<Request>
+    {
+        /**
+         * Constructs a new {@code RequestAdapter}.
+         * @param context Android Context
+         * @param resource resource ID
+         */
+        RequestAdapter(final Context context, final int resource)
+        {
+            super(context, resource);
+
+        }
+
+        @Override
+        public View getView(final int position, final View convertView, final ViewGroup parent)
+        {
+            final Request request = getItem(position);
+            final TextView view = (TextView) super.getView(position, convertView, parent);
+            view.setText(request.getRentalProvider().getName());
+            return view;
+        }
     }
 }
