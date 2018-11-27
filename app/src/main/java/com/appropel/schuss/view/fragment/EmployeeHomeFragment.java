513c6240b753b03e20935ecea1d2679fb3ae4b04
Kevin A. Roll <kroll@appropel.com>
2018-11-27 13:25:24 -0500

    Created layout and adapter for showing pending rental requests.

--- app/src/main/java/com/appropel/schuss/view/fragment/EmployeeHomeFragment.java
+++ app/src/main/java/com/appropel/schuss/view/fragment/EmployeeHomeFragment.java
@@ -5,18 +5,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.appropel.schuss.R;
 import com.appropel.schuss.common.util.EventBusFacade;
+import com.appropel.schuss.common.util.Preferences;
 import com.appropel.schuss.controller.SchussController;
 import com.appropel.schuss.controller.event.RequestEvent;
 import com.appropel.schuss.dagger.DaggerWrapper;
+import com.appropel.schuss.model.read.User;
+import com.appropel.schuss.view.util.RequestAdapter;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
@@ -25,6 +36,17 @@ import butterknife.Unbinder;
  */
 public final class EmployeeHomeFragment extends Fragment
 {
+    /** Logger. */
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeHomeFragment.class);
+
+    /** Name of provider at top header. */
+    @BindView(R.id.rental_provider_name)
+    TextView providerNameView;
+
+    /** List of rental requests. */
+    @BindView(R.id.rental_list)
+    ListView rentalView;
+
     /** Controller. */
     @Inject
     SchussController controller;
@@ -33,17 +55,44 @@ public final class EmployeeHomeFragment extends Fragment
     @Inject
     EventBusFacade eventBus;
 
+    /** Object mapper. */
+    @Inject
+    ObjectMapper objectMapper;
+
+    /** Preferences. */
+    @Inject
+    Preferences preferences;
+
+    /** Request view adapter. */
+    private RequestAdapter requestAdapter;
+
     /** View unbinder. */
     private Unbinder unbinder;
+
+    /** User data. */
+    private User user;  // NOPMD
 
     @Override
     public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState)
     {
-        final View view = inflater.inflate(R.layout.home, container, false);
+        final View view = inflater.inflate(R.layout.employee_home, container, false);
         unbinder = ButterKnife.bind(this, view);
         DaggerWrapper.INSTANCE.getComponent().inject(this);
 
-        return super.onCreateView(inflater, container, savedInstanceState);
+        requestAdapter = new RequestAdapter(getActivity());
+        rentalView.setAdapter(requestAdapter);
+
+        try
+        {
+            user = objectMapper.readValue(preferences.getUser(), User.class);
+        }
+        catch (IOException e)
+        {
+            LOGGER.error("JSON exception", e);
+        }
+        providerNameView.setText(user.getRentalProvider().getName());
+
+        return view;
     }
 
     @Override
@@ -61,7 +110,9 @@ public final class EmployeeHomeFragment extends Fragment
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void onRequestEvent(final RequestEvent event)
     {
-        // TODO
+        requestAdapter.clear();
+        requestAdapter.addAll(event.getRequests());
+        requestAdapter.notifyDataSetChanged();
     }
 
     @Override
---
+++ app/src/main/java/com/appropel/schuss/view/util/RequestAdapter.java
@@ -0,0 +1,53 @@
+package com.appropel.schuss.view.util;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.appropel.schuss.R;
+import com.appropel.schuss.model.read.Request;
+
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+/**
+ * An ArrayAdapter that displays info about a rental request.
+ */
+// TODO: convert this to recyclerview at some point. This class is written inefficiently because the data list
+// is expected to be small.
+public final class RequestAdapter extends ArrayAdapter<Request>
+{
+    /** Time formatter. */
+    private static final Format TIME_FORMAT = new SimpleDateFormat("HH:mm", Locale.getDefault());
+
+    /**
+     * Constructs a new {@code RequestAdapter}.
+     * @param context Android Context
+     */
+    public RequestAdapter(final Context context)
+    {
+        super(context, R.layout.rental_request_summary);
+    }
+
+    @Override
+    public View getView(final int position, final View convertView, final ViewGroup parent)
+    {
+        final Request request = getItem(position);
+        final View view = convertView != null ? convertView
+                : LayoutInflater.from(getContext()).inflate(R.layout.rental_request_summary, parent, false);
+
+        final TextView arrivalTimeView = view.findViewById(R.id.arrival_time_digits);
+        arrivalTimeView.setText(TIME_FORMAT.format(request.getArrivalTime()));
+        final TextView rentersView = view.findViewById(R.id.renters);
+        rentersView.setText(String.format("%d", request.getProfiles().size()));
+
+//        final TextView renterNamesView = view.findViewById(R.id.renter_names);
+//        StringBuilder sb = new StringBuilder();
+//        for ()
+        return view;
+    }
+}
